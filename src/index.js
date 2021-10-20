4d4f9f8cbb374a6be9b9b30785197f1761e2ef80
Dima-as <d.sichenka@gmail.com>
2021-10-20 20:05:46 +0300

    HTTP запрос

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import countriesTpl from "./hbs/countries.hbs";
+import listCountriesTpl from "./hbs/list-countries.hbs";
+import API from "./js/fetchCountries";
+import debounce from "lodash.debounce";
+import Notiflix from "notiflix";
+
+const DEBOUNCE_DELAY = 300;
+let searchQuery = "";
+
+const refs = {
+  containerСountry: document.querySelector(".country"),
+  input: document.querySelector(".input"),
+};
+
+refs.input.addEventListener(
+  "input",
+  debounce(() => {
+    inputSearchСountry();
+  }, 500)
+);
+
+function inputSearchСountry(evt) {
+  searchQuery = refs.input.value;
+  if (!searchQuery) {
+    clearCountriesCardMarkup();
+    return;
+  }
+
+  API.fetchCountries(searchQuery)
+    .then(characterSetInput)
+    .catch((error) => {
+      console.error();
+    });
+}
+
+function characterSetInput(countries) {
+  if (countries.length > 10) {
+    clearCountriesCardMarkup();
+    Notiflix.Notify.info(
+      "Too many matches found. Please enter a more specific name."
+    );
+  }
+  if (countries.length > 1 && countries.length <= 10) {
+    clearCountriesCardMarkup();
+    refs.containerСountry.innerHTML = listCountriesTpl(countries);
+  }
+  if (countries.length === 1) {
+    clearCountriesCardMarkup();
+    refs.containerСountry.innerHTML = countriesTpl(countries[0]);
+  }
+}
+
+function clearCountriesCardMarkup() {
+  refs.containerСountry.innerHTML = "";
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,9 @@
+const URL = "https://restcountries.com/v2/name/";
+fetchCountries();
+function fetchCountries(name) {
+  return fetch(`${URL}${name}`).then((response) => {
+    return response.json();
+  });
+  console.log(fetchCountries);
+}
+export default { fetchCountries };
